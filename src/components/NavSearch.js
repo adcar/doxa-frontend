681a21d061b38c8306341affbdc652bf444d3e0b
adcar <123zezima21@gmail.com>
2019-11-28 22:22:17 -0500

    Added navbar with sentiment search

---
+++ src/components/NavSearch.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import Router from "next/router";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  input: {
+    width: "100%",
+    transition: "all 0.1s ease-out",
+    paddingLeft: 20,
+    paddingRight: 20,
+    height: 30,
+    borderRadius: theme.radius,
+    backgroundColor: theme.bg,
+    border: "none",
+    outline: "none",
+    "&:focus": {
+      boxShadow: `3px 10px 15px rgba(0,0,0,0.25)`
+    },
+    "&::placeholder": {
+      color: "lightgrey"
+    }
+  },
+  submit: {
+    cursor: "pointer",
+    height: 30,
+    padding: "0px 15px",
+    position: "absolute",
+    left: 138,
+    transition: "all 0.1s ease-out",
+    borderRadius: theme.radius,
+    border: "none",
+    backgroundColor: theme.palette.primary.main,
+    color: "white",
+    outline: "none",
+    "&:hover": {
+      boxShadow: `3px 10px 15px ${theme.palette.primary.light}`
+    },
+    "&:focus": {
+      backgroundColor: theme.palette.primary.dark
+    }
+  },
+  form: {
+    position: "relative"
+  }
+}));
+
+export default function NavSearch() {
+  const classes = useStyles();
+  const [term, setTerm] = useState("");
+  return (
+    <form
+      className={classes.form}
+      onSubmit={e => {
+        e.preventDefault();
+        Router.push({
+          pathname: "/results",
+          query: { term }
+        });
+      }}
+    >
+      <input
+        className={classes.input}
+        type="text"
+        onChange={e => setTerm(e.target.value)}
+        placeholder="Bitcoin"
+        pattern="^[\w\s!#$']+$"
+        title="Only numbers, letters, dollar signs ($), and hashtags (#) are allowed"
+        required
+      />
+      <input className={classes.submit} type="submit" value="Analyze" />
+    </form>
+  );
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,38 @@
-import React, { useEffect, useState } from "react";
-import Router from "next/router";
+import React from "react";
+import { useRouter } from "next/router";
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import NavSearch from "./NavSearch";
+
+const useStyles = makeStyles(theme => ({
+  nav: {
+    height: 70,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-around",
+    boxShadow: `0px 4px 14px ${theme.shadowColor}`
+  }
+}));
 
 export default function Navbar() {
-  useEffect(() => {
-    Router.events.on("routeChangeComplete", () => {
-      console.log(Router.pathname);
-    });
-  }, []);
-  return (
-    <Typography variant="h3" style={{ backgroundColor: "#f0f0f0", margin: 0 }}>
-      Doxa
-    </Typography>
-  );
+  const router = useRouter();
+  const classes = useStyles();
+
+  if (router.pathname === "/") {
+    return (
+      <Typography
+        variant="h3"
+        style={{ backgroundColor: "#f0f0f0", margin: 0 }}
+      >
+        Doxa
+      </Typography>
+    );
+  } else {
+    return (
+      <nav className={classes.nav}>
+        <Typography>Doxa</Typography>
+        <NavSearch />
+      </nav>
+    );
+  }
 }
