3351bef04b6e2c614418a01bf1b658f3e59807cc
adcar <123zezima21@gmail.com>
2019-12-05 22:38:17 -0500

    Change color in the sentiment message

--- src/components/SentimentGauge.js
+++ src/components/SentimentGauge.js
@@ -135,7 +135,7 @@ export default function SentimentGauge({ value }) {
         <Typography className={classes.lowest}>-100</Typography>
         <Typography className={classes.highest}>100</Typography>
         <p className={classes.emoji}>{emoji}</p>
-        <Typography style={{ margin: 0, textAlign: "center" }}>
+        <Typography style={{ margin: 0, textAlign: "center", color }}>
           {sentiment}
         </Typography>
         {formattedValue}
