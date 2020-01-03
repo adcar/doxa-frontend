69f03c5447ccce58d5abaa3f29f39963923735da
adcar <alexanderdcardosi@gmail.com>
2020-01-03 02:22:10 -0500

    Customized background dynamically

--- src/theme.js
+++ src/theme.js
@@ -4,28 +4,38 @@ import { red } from "@material-ui/core/colors";
 // Create a theme instance.
 
 const shadowColor = "#967fac";
-const theme = createMuiTheme({
-  radius: 50,
-  shadowColor: "#967fac",
-  purpleShadow: `5px 10px 15px ${shadowColor}`,
-  palette: {
-    type: "light",
-    primary: {
-      main: "#7E1DEC"
+
+function getTheme(theme) {
+  return createMuiTheme({
+    radius: 50,
+    shadowColor: "#967fac",
+    purpleShadow: `5px 10px 15px ${shadowColor}`,
+    palette: {
+      type: theme.paletteType,
+      primary: {
+        main: "#7E1DEC"
+      },
+      secondary: {
+        main: "#d24ae8"
+      },
+      neutral: {
+        main: "#9C27B0"
+      },
+      error: {
+        main: red.A400
+      },
+      background: {
+        default: theme.paletteType === "light" ? "#f7f7f7" : "#2f2f31"
+      }
     },
-    secondary: {
-      main: "#d24ae8"
-    },
-    neutral: {
-      main: "#9C27B0"
-    },
-    error: {
-      main: red.A400
+    typography: {
+      fontFamily: ['"Montserrat"'].join(",")
     }
-  },
-  typography: {
-    fontFamily: ['"Montserrat"'].join(",")
-  }
+  });
+}
+
+const theme = getTheme({
+  paletteType: "dark"
 });
 
 export default responsiveFontSizes(theme);
