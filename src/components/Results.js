09bd036c6dd682155c0b77db1ed7de3e7a67c8f8
adcar <alexanderdcardosi@gmail.com>
2020-01-07 00:31:56 -0500

    Removed the word "term" from loader

--- src/components/Results.js
+++ src/components/Results.js
@@ -87,7 +87,7 @@ export default function Results({ term }) {
           align="center"
           className={classes.spinnerLabel}
         >
-          Analyzing Tweets for term{" "}
+          Analyzing Tweets for{" "}
           <Typography variant="inherit" color="primary">
             "{term}"
           </Typography>
